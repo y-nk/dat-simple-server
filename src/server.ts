66880a79ee5d9b8adadb14004c8e8a71fa4f872e
Julien Barbay <julien.barbay@gmail.com>
2021-05-23 17:03:47 +0700

    add bodyparser json as default

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import { json } from 'body-parser'
 import middleware from './middleware'
 
 export default (root: string, port: number = 8001, host: string = '0.0.0.0') => {
   const app = express()
+  app.use(json())
+  app.use('/', express.static(root))
   app.use('/', middleware(root))
 
   app.listen(port, host, () => {
