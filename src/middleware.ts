1e14bdd9eac48b243e1638f952669afc0eadd521
Julien Barbay <julien.barbay@gmail.com>
2021-05-23 15:41:35 +0700

    simplify

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,40 @@
+declare function require(module: string): any;
+
+import { join } from 'path'
 import { RequestListener, IncomingMessage, ServerResponse } from 'http'
-import thread from './thread'
+import { runInNewContext } from 'vm'
+
+const payload = (file: string): string => (`
+  (async () => {
+    try {
+      const lambda = require('${file}')
+      await lambda(req, res)
+    } catch (error) {
+      switch(error.code) {
+        case 'MODULE_NOT_FOUND':
+          res.statusCode = 404
+          res.end('Not Found' + ' --- ' + error.toString())
+          break
+
+        default:
+          res.statusCode = 500
+          res.end('Server error' + ' --- ' + error.toString())
+          break
+      }
+    }
+  })()
+`)
 
 export default (root: string, timeout = 30000): RequestListener => (req: IncomingMessage, res: ServerResponse) => {
-  try {
-    const [path] = req.url!.split('?')
-    thread(`${root}${path}`, req, res, timeout)
-  }
-  catch(error) {
-    switch(error.code) {
-      case 'MODULE_NOT_FOUND':
-        res.statusCode = 404
-        res.end(`Not Found - ${error}`)
-        break
+  const [path] = req.url!.split('?')
 
-      default:
-        res.statusCode = 500
-        res.end(`Server Error - ${error}`)
-        break
-    }
-  }
+  // forbid ../ for path escalation
+  const file = join(root, path.replace(/\.\.?\//g, ''))
+
+  return runInNewContext(
+    payload(file), {
+      require,
+      req, res
+    }, { timeout }
+  )
 }
