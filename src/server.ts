47fef186898feaf37179e305cf951f4eba3391f5
Julien Barbay <julien.barbay@gmail.com>
2021-05-23 15:41:16 +0700

    add host as option

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import http from 'http'
 import middleware from './middleware'
 
-export default (root: string, port: number): http.Server => (
-  http.createServer(middleware(root)).listen(port)
+export default (root: string, port: number = 8001, host: string = '0.0.0.0'): http.Server => (
+  http.createServer(middleware(root)).listen(port, host, () => {
+    console.log('\n', `💥 Dat simple server serving ${root}/ at http://${host}:${port}`, '\n')
+  })
 )
